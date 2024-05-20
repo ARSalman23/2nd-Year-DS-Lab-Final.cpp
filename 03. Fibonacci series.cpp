61cb70ce91b3cf9980e613ee4ad979b036a819b9
Md Abdur Rahman Salman <139158260+ARSalman23@users.noreply.github.com>
2024-05-20 21:06:13 +0600

    Update 03. Fibonacci series.cpp

--- 03. Fibonacci series.cpp
+++ 03. Fibonacci series.cpp
@@ -20,4 +20,37 @@ int main()
     }
 }
 
+/*
+#include<bits/stdc++.h>
+using namespace std;
 
+typedef long long ll;
+#define endl "\n"
+#define mem(array, b) memset(array, b, sizeof(array))
+const int mx = 128;
+ll dp[mx];
+
+ll fib(ll n)
+{
+    if(n == 1) return 1;
+    if(dp[n] != -1) return dp[n];
+    dp[n] = fib(n - 1) + fib(n - 2);
+    return dp[n];
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    mem(dp, -1);
+
+    ll n;
+    cin >> n;
+
+    ll ans = fib(n);
+    cout << ans << endl;
+
+    return 0;
+}
+*/
