79e0ccc2758b74cbf10c9b83ba9c5ca7144cb22e
Md Abdur Rahman Salman <139158260+ARSalman23@users.noreply.github.com>
2024-05-20 20:48:20 +0600

    Create 3. Fibonacci series.cpp

---
+++ 3. Fibonacci series.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+int f(int n)
+{
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    return f(n - 1) + f(n - 2);
+}
+
+int main()
+{
+    cout << 0 << ' ';
+    for(int i = 1; i < 10; i++)
+    {
+        int x = f(i);
+        cout << x << ' '; /// 0 1 1 2 3 5 8 13 21 34
+    }
+}
+
+
